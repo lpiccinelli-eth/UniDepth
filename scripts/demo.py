b43718ae216c67e9c4b56ad222f176962a6de1af
RoyYang0714 <sy2es93104@gmail.com>
2024-04-04 17:37:16 +0200

    ammend fwd

--- scripts/demo.py
+++ scripts/demo.py
@@ -7,16 +7,12 @@ from unidepth.utils import colorize, image_grid
 
 def demo(model):
     rgb = np.array(Image.open("assets/demo/rgb.png"))
-    import cv2
-    rgb = cv2.resize(rgb, (616, 462))
     rgb_torch = torch.from_numpy(rgb).permute(2, 0, 1)
     intrinsics_torch = torch.from_numpy(np.load("assets/demo/intrinsics.npy"))
 
     # predict
-    # predictions = model.infer(rgb_torch, intrinsics_torch)
-    with torch.no_grad():
-        predictions = model({"image": rgb_torch.unsqueeze(0).float().cuda(), "K": intrinsics_torch.unsqueeze(0).cuda()}, {})
-    print(predictions.keys())
+    predictions = model.infer(rgb_torch, intrinsics_torch)
+
     # get GT and pred
     depth_pred = predictions["depth"].squeeze().cpu().numpy()
     depth_gt = np.array(Image.open("assets/demo/depth.png")).astype(float) / 1000.0
@@ -41,7 +37,7 @@ def demo(model):
 if __name__ == "__main__":
     print("Torch version:", torch.__version__)
     model = torch.hub.load(
-        "lpiccinelli-eth/unidepth:feat/fwd",
+        "lpiccinelli-eth/unidepth",
         "UniDepthV1_ViTL14",
         pretrained=True,
         trust_repo=True,
