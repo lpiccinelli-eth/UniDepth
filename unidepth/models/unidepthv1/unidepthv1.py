87ee724d5c548effca2e751af403bf0aae0721d3
Niels <niels.rogge1@gmail.com>
2024-04-06 14:55:23 +0200

    More improvements

--- unidepth/models/unidepthv1/unidepthv1.py
+++ unidepth/models/unidepthv1/unidepthv1.py
@@ -350,7 +350,7 @@ class UniDepthV1(nn.Module):
 
 
 class UniDepthV1HF(UniDepthV1, PyTorchModelHubMixin,
-                   library_name="keras-nlp",
+                   library_name="UniDepth",
                    repo_url="https://github.com/lpiccinelli-eth/UniDepth",
                    docs_url="https://github.com/lpiccinelli-eth/UniDepth?tab=readme-ov-file#unidepth-universal-monocular-metric-depth-estimation",):
     def __init__(self, config):
