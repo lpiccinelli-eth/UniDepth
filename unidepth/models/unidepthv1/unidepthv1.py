680608ac55ca0a6a5049cda5b6dff29af4713c4c
RoyYang0714 <sy2es93104@gmail.com>
2024-04-11 10:28:42 +0200

    added readme

--- unidepth/models/unidepthv1/unidepthv1.py
+++ unidepth/models/unidepthv1/unidepthv1.py
@@ -340,13 +340,7 @@ class UniDepthV1(nn.Module):
         self.image_shape = config["data"]["image_shape"]
 
 
-class UniDepthV1HF(
-    UniDepthV1,
-    PyTorchModelHubMixin,
-    # library_name="UniDepth",
-    # repo_url="https://github.com/lpiccinelli-eth/UniDepth",
-    # tags=["monocular-metric-depth-estimation"],
-):
+class UniDepthV1HF(UniDepthV1, PyTorchModelHubMixin):
     def __init__(self, config):
         super().__init__(config)
 
